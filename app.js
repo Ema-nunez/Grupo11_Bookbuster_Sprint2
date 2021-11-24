19ce66a5a0e65ddde792e3348471fc96b0faf711
jhonatannunez <jhonatanchipana4@gmail.com>
2021-11-24 13:47:37 -0300

    modificaciones generales

--- app.js
+++ app.js
@@ -21,3 +21,7 @@ app.get("/register", (req, res) => {
 app.get("/cart", (req, res) => {
   res.sendFile(__dirname + "/views/cart.html");
 });
+
+app.get("/product", (req, res) => {
+  res.sendFile(__dirname + "/views/productDetail.html");
+});
